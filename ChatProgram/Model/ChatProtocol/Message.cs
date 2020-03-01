b8f537eec5ac0c8ab9f8af072e74fa01ed2579ca
sdgsdg21345324 <xocnsdl2013@gmail.com>
2020-03-02 03:43:18 +0900

    - 수정

--- ChatProgram/Model/ChatProtocol/Message.cs
+++ ChatProgram/Model/ChatProtocol/Message.cs
@@ -64,48 +64,41 @@ namespace ChatProgram.Model.ChatProtocol
 
         }
 
-        public async Task<MessageHeader> ReadMessageHeader(NetworkStream stream)
+        public Task<MessageHeader> ReadMessageHeader(NetworkStream stream)
         {
             MessageHeader messageHeader = new MessageHeader();
-            await Task.Run(() =>
+            byte[] buffer = new byte[8];
+            while (true)
             {
-                byte[] buffer = new byte[8];
-                while (true)
+                int byteLength = stream.Read(buffer, 0, 8);
+                if (byteLength >= 8)
                 {
-                    int byteLength = stream.Read(buffer, 0, 8);
-                    if (byteLength >= 8)
-                    {
-                        // 읽어온 메시지 헤더 byte배열 분리후 MessageHeader 구조체로 변환
-                        byte[] msgTypeBytes = new byte[4];
-                        byte[] msgBodyLengthBytes = new byte[4];
-                        Array.Copy(buffer, 0, msgTypeBytes, 0, 4);
-                        Array.Copy(buffer, 4, msgBodyLengthBytes, 0, 4);
+                    // 읽어온 메시지 헤더 byte배열 분리후 MessageHeader 구조체로 변환
+                    byte[] msgTypeBytes = new byte[4];
+                    byte[] msgBodyLengthBytes = new byte[4];
+                    Array.Copy(buffer, 0, msgTypeBytes, 0, 4);
+                    Array.Copy(buffer, 4, msgBodyLengthBytes, 0, 4);
 
-                        messageHeader.MSGTYPE = BitConverter.ToUInt32(msgTypeBytes, 0);
-                        messageHeader.BODYLEN = BitConverter.ToUInt32(msgBodyLengthBytes, 0);
-                    }
+                    messageHeader.MSGTYPE = BitConverter.ToUInt32(msgTypeBytes, 0);
+                    messageHeader.BODYLEN = BitConverter.ToUInt32(msgBodyLengthBytes, 0);
+                    return messageHeader;
                 }
-            });
-            return messageHeader;
+            }
         }
 
         public async Task<string> ReadMessageBody(NetworkStream stream, uint bodyLength)
         {
             string msg = string.Empty;
-            await Task.Run(() =>
+            byte[] buffer = new byte[bodyLength];
+            while (true)
             {
-                byte[] buffer = new byte[bodyLength];
-                while (true)
+                int byteLength = stream.Read(buffer, 0, Convert.ToInt32(bodyLength));
+                if (byteLength >= bodyLength)
                 {
-                    int byteLength = stream.Read(buffer, 0, Convert.ToInt32(bodyLength));
-                    if (byteLength >= bodyLength)
-                    {
-                        msg = Encoding.UTF8.GetString(buffer, 0, byteLength);
-                    }
+                    msg = Encoding.UTF8.GetString(buffer, 0, byteLength);
+                    return msg;
                 }
-            });
-            
-            return msg;
+            }
         }
 
         public void SendMessage(uint msgType, string message, NetworkStream stream)
