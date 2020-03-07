6ebedaa4be567dedcb609884e916e5c0cb2f0c0f
sdgsdg21345324 <xocnsdl2013@gmail.com>
2020-03-07 18:46:07 +0900

    - 유저 프로퍼티 변경 알림 버그 수정

--- ChatProgram/Model/Network/ServerControll.cs
+++ ChatProgram/Model/Network/ServerControll.cs
@@ -220,24 +220,24 @@ namespace ChatProgram.Model.Network
         private void REQ_CHANGE_NICKNAME(ConnectController controller, string msg)
         {
             string message = $"[서버] '{controller.UserNickname}'유저의 닉네임이 '{msg}'로 변경되었습니다.";
-            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             controller.UserNickname = msg;
+            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             MessageUtil.Instance.SendMessage(SEND_TO_CLIENT_DEFINE.SEND_SUCCESE_CHANGENICKNAME, sendMessage, controller.transmitStream);
         }
 
         private void REQ_CHANGE_NICKNAME_COLOR(ConnectController controller, string msg)
         {
             string message = $"[서버] '{controller.UserNicknameColor}'유저의 닉네임 색상이 '{msg}'로 변경되었습니다.";
-            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             controller.UserNicknameColor = msg;
+            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             MessageUtil.Instance.SendMessage(SEND_TO_CLIENT_DEFINE.SEND_SUCCESE_CHANGENICKNAMECOLOR, sendMessage, controller.transmitStream);
         }
 
         private void REQ_CHANGE_CHAT_COLOR(ConnectController controller, string msg)
         {
             string message = $"[서버] '{controller.UserChatColor}'유저의 채팅 색상이 '{msg}'로 변경되었습니다.";
-            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             controller.UserChatColor = msg;
+            string sendMessage = $"{controller.UserNickname},{controller.UserNicknameColor},{controller.UserChatColor},{message}";
             MessageUtil.Instance.SendMessage(SEND_TO_CLIENT_DEFINE.SEND_SUCCESE_CHANGECHATCOLOR, sendMessage, controller.transmitStream);
         }
 
